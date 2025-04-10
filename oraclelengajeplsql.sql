a99766bb37cdfa38c969d346475b1110987248f8
jhonnferrerosa <jhonn.trucke@gmail.com>
2025-04-10 20:04:15 +0200

    fin del sia

--- oraclelengajeplsql.sql
+++ oraclelengajeplsql.sql
@@ -34,4 +34,274 @@ END;
 UNDEFINE dato;
 UNDEFINE deptno;
 
+SELECT * from dept;
+
+declare 
+  v_numero dept.DEPT_NO%TYPE;
+  v_nombre dept.dnombre%TYPE;
+  v_localidad dept.LOC%TYPE;
+BEGIN
+  -- le pedimos al usuario los valores. 
+  v_numero := &iddepartamento;
+  v_nombre := '&nombre;';
+  v_localidad := '&localidad;';
+
+  INSERT into dept values (v_numero, v_nombre, v_localidad);
+end;
+
+UNDEFINE iddepartamento;
+UNDEFINE nombre;
+UNDEFINE localidad;
+
+--10/4/2025
+--ejercicios de sintaxisu 
+
+--2
+INSERT INTO emp VALUES('7839', 'rey', 'PRESIDENTE', NULL, TO_DATE('17-11-2005', 'DD-MM-YYYY'), 650000, NULL, 10);
+
+SELECT * from emp;
+
+DECLARE
+  v_empno emp.emp_no%TYPE;
+BEGIN
+  v_empno := &empno;
+  INSERT into emp values (v_empno, 'perez', 'analista', null, sysdate, 2000, null, 10);
+END;
+
+UNDEFINE empno;
+
+--3 
+SELECT * from emp;
+
+DECLARE
+  v_oficio emp.oficio%TYPE;
+  miValorSumar integer := 200;
+BEGIN
+  v_oficio := '&oficio';
+  UPDATE emp SET salario = salario + miValorSumar WHERE oficio = v_oficio;
+  dbms_output.put_line ('Se han actualizado los salarios de los empleados con oficio ' || v_oficio);
+END;
+
+--4  
+DECLARE
+  v_mientero integer;
+  mistring varchar2(100);
+BEGIN
+  v_mientero := &mientero;
+  mistring := to_char(v_mientero);
+  dbms_output.put_line ('La cantidad de cifras es: ' || length (mistring));
+END;
+
+UNDEFINE mientero;
+
+--5 ya esta hecho. 
+
+
+--6 
+SELECT * from emp;
+SELECT emp_no, nvl (COMISION, 0) from emp;
+--buscar a los empleados que tiene una comisión de más del 5%. 
+SELECT * from emp where nvl(comision,0)/SALARIO > 0.05;
+
+DECLARE 
+BEGIN
+  update emp set SALARIO = SALARIO * 1.05 where nvl(comision,0)/SALARIO < 0.05;
+end;
+
+--7
+
+DECLARE
+  miFecha date := sysdate;
+  v_cantidadmeses integer := 0;
+BEGIN
+  v_cantidadmeses := '&cantidadmeses';
+  miFecha := add_months (miFecha, v_cantidadmeses);
+  dbms_output.put_line ('La fecha hoy es: ' || miFecha);
+end;
+
+--8
+
+DECLARE
+  v_numerouno integer := 0;
+  v_numerodos integer := 0;
+
+BEGIN
+  v_numerouno := '&numerouno';
+  v_numerodos := '&numerodos';
+  dbms_output.put_line ('El resto de la división es:  ' || mod(v_numerouno, v_numerodos));
+end;
+
+
+--9
+DECLARE
+  v_minombre varchar2(100);
+BEGIN
+  v_minombre := '&minombre';
+  dbms_output.put_line ('El nombre es: ' || initcap(v_minombre));
+end;
+
+UNDEFINE minombre;
+
+
+--10
+SELECT * from emp;
+
+DECLARE
+  v_numerempleado emp.emp_no%TYPE;
+BEGIN
+  v_numerempleado := '&numerodeempleado';
+  DELETE FROM emp WHERE emp_no = v_numerempleado;
+end;
+
+
+UNDEFINE numerodeempleado;
+
+
+---  aquí van  ir las estructuras de control.
+--- en el texto es en el que tienes que poner las comillas, en el caso de que sea un entero,entonces no hace falta tener las comillas.
+
+declare 
+  v_minumerouno integer;
+BEGIN
+  v_minumerouno := '&minumerouno';
+
+  if (v_minumerouno > 0) then
+    dbms_output.put_line ('El número es positivo ');
+  elsif (v_minumerouno = 0) then  
+    dbms_output.put_line ('El número es cero ');
+  else
+    dbms_output.put_line ('El número es negativo ');
+  end if;
+end;
+
+UNDEFINE minumerouno;
+
+
+declare 
+  v_numero integer;
+BEGIN
+  v_numero := &numero;
+  if (v_numero = 1) then
+    dbms_output.put_line ('es primavera');
+  elsif (v_numero = 2) then
+    dbms_output.put_line ('es verano');
+  ELSIF (v_numero = 3) then
+    dbms_output.put_line ('es otoño');
+  ELSIF (v_numero = 4) then
+    dbms_output.put_line ('es invierno');
+  else
+    dbms_output.put_line ('no es una estación válida');
+  end if;
+end;
+
+UNDEFINE numero;
+
+
+---ejemplo número mayor.
+--hacer
+
+
+---pedir un número al usuario y decir si es par o impar. 
+
+declare
+    v_numero integer;
+begin
+    v_numero := &numero;
+    
+    if (mod (v_numero,2) = 0) then
+        dbms_output.put_line (' es par. ');
+    else
+        dbms_output.put_line (' es impar.');
+    end if;
+end;
+
+-- en el caso de que sea vocal indiarlo, en el caso de que no, es consonante. 
+
+/
+declare 
+    v_micarater varchar2 (1);
+begin
+    v_micarater := '&micaracter';
+    v_micarater := lower (v_micarater);
+    if ((v_micarater = 'a') or (v_micarater = 'e') or (v_micarater = 'i') or (v_micarater = 'o') or (v_micarater = 'u'))  then
+        dbms_output.put_line ('es vocal');
+    else
+        dbms_output.put_line (' es consonante.');
+    end if;
+end;
+/
+
+
+
+declare 
+    v_minumerouno integer;
+    v_minumerodos integer;
+    v_minumerotres integer;
+begin
+    v_minumerouno := &minumerouno;
+    v_minumerodos := &minumerodos;
+    v_minumerotres := &minumerotres;
+    if (v_minumerouno > v_minumerodos) and (v_minumerouno > v_minumerotres) then
+        dbms_output.put_line ('elmayor es el primero');
+    elsif (v_minumerodos > v_minumerouno) and (v_minumerodos > v_minumerotres) then
+        dbms_output.put_line ('elmayor es el segundo');
+    else
+        dbms_output.put_line ('elmayor es el tercero');
+    end if;    
+    if (v_minumerouno < v_minumerodos) and (v_minumerouno < v_minumerotres) then
+        dbms_output.put_line ('el menor es el primero');
+    elsif (v_minumerodos < v_minumerouno) and (v_minumerodos < v_minumerotres) then
+        dbms_output.put_line ('el menor es el segundo');
+    else
+        dbms_output.put_line ('el menor es el tercero');
+    end if;
+end;
+
+
+declare 
+    v_minumerouno integer;
+    v_minumerodos integer;
+    v_minumerotres integer;
+    v_maximo integer;
+    v_minimo integer;
+    v_intermedio integer;
+begin
+    v_minumerouno := &minumerouno;
+    v_minumerodos := &minumerodos;
+    v_minumerotres := &minumerotres;
+    if (v_minumerouno > v_minumerodos) and (v_minumerouno > v_minumerotres) then
+        dbms_output.put_line ('elmayor es el primero');
+        v_maximo := v_minumerouno;
+    elsif (v_minumerodos > v_minumerouno) and (v_minumerodos > v_minumerotres) then
+        dbms_output.put_line ('elmayor es el segundo');
+        v_maximo := v_minumerodos;
+    else
+        dbms_output.put_line ('elmayor es el tercero');
+        v_maximo := v_minumerotres;
+    end if;    
+    if (v_minumerouno < v_minumerodos) and (v_minumerouno < v_minumerotres) then
+        dbms_output.put_line ('el menor es el primero');
+        v_minimo := v_minumerouno;
+    elsif (v_minumerodos < v_minumerouno) and (v_minumerodos < v_minumerotres) then
+        dbms_output.put_line ('el menor es el segundo');
+        v_minimo := v_minumerodos;
+    else
+        dbms_output.put_line ('el menor es el tercero');
+        v_minimo := v_minumerotres;
+    end if;
+    if (v_maximo != v_minumerouno) and (v_minimo != v_minumerouno) then
+        dbms_output.put_line ('el intermedio es el primero');
+    elsif (v_maximo != v_minumerodos) and (v_minimo != v_minumerodos) then
+        dbms_output.put_line ('el intermedio es el segundo');
+    else
+        dbms_output.put_line ('el intermedio es el terero');
+    end if;
+end;
+
+
+
+
+
+
+
 
